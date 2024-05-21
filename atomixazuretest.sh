ecef15c322c263ec5d208d3f857a81dbd1553bfd
Long T. Kira <84077194+kirazero17@users.noreply.github.com>
2024-05-21 16:04:06 +0700

    Testing Atomix with it

---
+++ atomixazuretest.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+MSYS_NO_PATHCONV=1
+az login --user "${email}" --password "${password}"
+
+# specify CREATOR
+NAME="lghg-$RANDOM"
+AZURE_RESOURCE_GROUP="${NAME}-group"
+
+#  westus2 can be changed to any available location (`az account list-locations`)
+az group create --name "${AZURE_RESOURCE_GROUP}" -l centralindia
+
+az network vnet create \
+    --resource-group "${AZURE_RESOURCE_GROUP}" \
+    --name "${NAME}-cluster-net" \
+    --address-prefixes 192.168.30.0/24 \
+    --subnet-name "${NAME}-node-subnet" \
+    --subnet-prefix 192.168.30.0/24
+
+# Store the ID of the created subnet
+NODE_SUBNET_ID=$(az network vnet subnet show \
+    --resource-group "${AZURE_RESOURCE_GROUP}" \
+    --vnet-name "${NAME}-cluster-net" \
+    --name "${NAME}-node-subnet" \
+    --query id \
+    -o tsv)
+
+az aks create \
+    --resource-group "${AZURE_RESOURCE_GROUP}" \
+    --name "${NAME}" \
+    --network-plugin azure \
+	--network-plugin-mode overlay \
+    --pod-cidr "10.10.0.0/16" \
+    --service-cidr "10.11.0.0/16" \
+    --dns-service-ip "10.11.0.10" \
+    --vnet-subnet-id "${NODE_SUBNET_ID}" \
+    -s standard_d2as_v4 -c 1
+
+az aks get-credentials \
+    --resource-group "${AZURE_RESOURCE_GROUP}" \
+    --name "${NAME}"
+
+helm install -n kube-system atomix ../../Helm/atomix-helm-charts/atomix
